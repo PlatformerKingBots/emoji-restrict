29c33b4131c6214e14ff0a2bed757635f2295a1c
PlatformerKingBots <45886615+PlatformerKingBots@users.noreply.github.com>
2019-02-13 09:36:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,24 @@ client.on('message', message => {
           }, 1000);
         });
     }
+    if (message.content.startsWith('e$eval')&&message.author.id === "270997352939126794") {
+        try {
+          const code = args.join(" ");
+          let evaled = eval(code);
     
+          if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+    
+          message.channel.send(clean(evaled), {code:"xl"});
+        } catch (err) {
+          message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+    }
 });//Message
-
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
 client.login(process.env.token);
