5d8e7668dc0e09593f71d4a441c9c41904f306a4
PlatformerKingBots <45886615+PlatformerKingBots@users.noreply.github.com>
2019-02-12 22:11:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,75 @@
-const Discord = require('discord.js');
+//https://discordapp.com/oauth2/authorize?client_id=545069699570860032&scope=bot&permissions=1074089024
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-var toasters = [
-  'https://media.giphy.com/media/71loenMVZGgN2/giphy.gif',
-  'https://media.giphy.com/media/VWVthRb92CS8E/giphy.gif',
-  'https://media.giphy.com/media/71loenMVZGgN2/giphy.gif',
-  'https://media.giphy.com/media/7wUoPcTuC166s/giphy.gif',
-  'https://media.giphy.com/media/EAvAOsVRNphfO/giphy.gif',
-  'https://media1.tenor.com/images/84e2ba1c9804ecaf13b24a776a03f03a/tenor.gif?itemid=4950420',
-  'https://media1.tenor.com/images/e7fe8d76d9f1cf392c2d97d87d673f07/tenor.gif?itemid=5319546',
-  'https://media1.tenor.com/images/e700a457fb705fd1b90e65baab24c5ba/tenor.gif?itemid=5645503',
-  'https://media1.tenor.com/images/e700a457fb705fd1b90e65baab24c5ba/tenor.gif?itemid=5645503',
-  'https://media1.tenor.com/images/73d563cdc282ad270a027f8d5efb17a8/tenor.gif?itemid=5395765',
-  'https://media1.tenor.com/images/5b6a39aa00312575583031d2de4edbd4/tenor.gif?itemid=5468293',
-  'https://media1.tenor.com/images/36840fcc3b6aba202c3f75811a7262d1/tenor.gif?itemid=10874237',
-];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-  if (message.content.startsWith('TOASTER')){
-    message.channel.send({file: toasters[Math.floor(Math.random() * toasters.length)]});
-  }
-});
 
-client.login(process.env.token);
+client.on('message', message => {
+    let args = message.content.split(" ").splice(1);
+    let m = message.content.toLowerCase();
+    if (message.author === client.user) {return;}
+    if (message.content.startsWith('e$help')) {
+      message.channel.send({embed: {
+        title: 'EmojiRestrictBot Help',
+        color: 0xf1c40f,
+        fields: [
+          {name: 'e$add', value: 'Adds a restricted role.\nUsage: `e$add <:smile:545074099890683914> @Role`'},
+          {name: 'e$remove', value: 'Removes a restricted role.\nUsage: `e$remove <:smile:545074099890683914> @Role`'},
+          {name: 'e$invite', value: 'Provides you with the bot invite link.\nUsage: `e$invite`'},
+        ],
+      }});
+    }
+    if (message.content.startsWith('e$invite')) {
+      message.channel.send({embed: {
+        title: 'Invite Link',
+        color: 0xf1c40f,
+        description: '[Invite me to your server](https://discordapp.com/oauth2/authorize?client_id=545069699570860032&scope=bot&permissions=1074089024)'
+      }});
+    }
+    if (message.content.startsWith('e$add') && message.member.hasPermission('MANAGE_EMOJI')) {
+        var n = args[0].split(':')[1];
+        var emoji = message.guild.emojis.find('name', n);
+        if (!emoji) {
+          message.channel.send(`<:error:487316007040581632> ${message.member}, Invalid emoji.`);
+          return;
+        }
+        var role = message.guild.roles.get(args[2]);
+        if (!role) {role = message.mentions.roles.first();}
+        if (!role) {
+          message.channel.send(`<:error:487316007040581632> ${message.member}, Invalid role.`);
+          return;
+        }
+        emoji.addRestrictedRole(role);
+        message.channel.send('Adding restricted role...').then(msg => {
+          setTimeout(function() {
+            msg.edit('<:success:487316006843449354> Restricted role added.');
+          }, 1000);
+        });
+    }
+    if (message.content.startsWith('e$remove') && message.member.hasPermission('MANAGE_EMOJI')) {
+        var n = args[0].split(':')[1];
+        var emoji = message.guild.emojis.find('name', n);
+        if (!emoji) {
+          message.channel.send(`<:error:487316007040581632> ${message.member}, Invalid emoji.`);
+          return;
+        }
+        var role = message.guild.roles.get(args[2]);
+        if (!role) {role = message.mentions.roles.first();}
+        else if (!role) {
+          message.channel.send(`<:error:487316007040581632> ${message.member}, Invalid role.`);
+          return;
+        }
+        emoji.removeRestrictedRole(role);
+        message.channel.send('Removing restricted role...').then(msg => {
+          setTimeout(function() {
+            msg.edit('<:success:487316006843449354> Restricted role removed.');
+          }, 1000);
+        });
+    }
+    
+});//Message
+
+client.login('NTQ1MDY5Njk5NTcwODYwMDMy.D0UTdQ.ch9NzclLSw-iSSJ0Z9KkvB6NnxI');
